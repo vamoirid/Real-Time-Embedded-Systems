ad4d4de242a46b54ab1970fb95102460096e13b8
Vasileios Amoiridis <vassilisamir@gmail.com>
2020-03-17 03:48:13 +0200

    Added primary queue functions

--- prod-cons.c
+++ prod-cons.c
@@ -24,7 +24,7 @@ typedef struct {
 	int buf[QUEUESIZE];
 	long head, tail;
 	int full, empty;
-	pthread_mutex_t *mutex;
+	pthread_mutex_t *mut;
 	pthread_cond_t *notFull, *notEmpty;
 	sem_t *sem;
 } queue;
@@ -44,7 +44,7 @@ void *consumer(void *args);
  */
 queue queueInit();
 void queueAdd(queue *q, int in);
-void queueDel(queue *q, int out);
+void queueDel(queue *q, int *out);
 void queueDelete(queue *q);
 
 queue queueInit()
@@ -68,5 +68,45 @@ queue queueInit()
 	sem_init(q->sem, 0, 0);
 
 	return (q);
+}
 
+void queueAdd(queue *q, int in)
+{
+	q->buf[q->tail] = in;
+	q->tail++;
+
+	if (q->tail == QUEUESIZE)
+		q->tail = 0;
+	if (q->tail == q->head)
+		q->full = 1;
+	q->empty = 0;
+
+	return;
+}
+
+void queueDel(queue *q, int *out)
+{
+	*out = q->buf[q->head];
+	q->head++;
+
+	if (q->head == QUEUESIZE)
+		q->head = 0;
+	if (q->head == q->tail)
+		q->empty = 1;
+	q->full = 0;
+
+	return;
+}
+
+void queueDelete(queue *q)
+{
+	pthread_mutex_destroy(q->mut);
+	free(q->mut);
+	pthread_cond_destroy(q->notFull);
+	free(q->notFull);
+	pthread_cond_destroy(q->notEmpty);
+	free(q->notEmpty);
+	sem_destroy(q->sem);
+	free(q->sem);
+	free(q);
 }
